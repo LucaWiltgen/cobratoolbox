17e25713f70da11306ca86cd704712460de136b3
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-17 10:50:33 +0200

    Adding git to path

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -10,6 +10,8 @@ elif [ "$ARCH" == "Windows" ]; then
     echo " -- killing the sh process --"
     nohup "C:\\Windows\\System32\\cmd.exe" /c "SET PATH=%PATH%;"
     "C:\\Windows\\System32\\cmd.exe" /c "echo %PATH%"
+    nohup "C:\\Windows\\System32\\cmd.exe" /c "SET PATH=C:\Program Files\Git\cmd;C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin;%PATH%;"
+    "C:\\Windows\\System32\\cmd.exe" /c "echo %PATH%"
     #nohup "C:\\Windows\\System32\\cmd.exe" /c "taskkill /im ssh-agent.exe /f /fi \"memusage gt 40\" 2>NUL | findstr SUCCESS >NUL && if errorlevel 1 ( echo ssh-agent was not killed ) else ( echo ssh-agent was killed )"
     #nohup "C:\\Windows\\System32\\cmd.exe" /c "taskkill /im sh.exe /f /fi \"memusage gt 40\" 2>NUL | findstr SUCCESS >NUL && if errorlevel 1 ( echo sh was not killed ) else ( echo sh was killed )"
 
