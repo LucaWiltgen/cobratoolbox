4840be5b4ff33156f0d2dc86f144bb85c426c677
laurentheirendt <laurent.heirendt@uni.lu>
2017-08-19 14:20:47 +0200

    Git bash and tail

--- .ci/runtests.sh
+++ .ci/runtests.sh
@@ -38,7 +38,7 @@ elif [ "$ARCH" == "Windows" ]; then
     #nohup "C:\\Program Files\\Matlab\\$MATLAB_VER\\\bin\\matlab.exe"  -useStartupFolderPref -logfile output.log -wait -r "system('which git'); system('git --version'); system('whoami'); system('pwd'); initCobraToolbox" & PID=$! #cd test; testAll;
     #"C:\\Program Files\\Matlab\\R2016b\\\bin\\matlab.exe" -nojvm -nodesktop -nosplash -useStartupFolderPref -logfile output.log -wait -r "initCobraToolbox;" -nojvm -nodesktop -nosplash
     # follow the log file
-    #tail -n0 -F --pid=$! output.log 2>/dev/null
+    tail -n0 -F --pid=$! output.log 2>/dev/null
 
     # wait until the background process is done
     wait $PID
